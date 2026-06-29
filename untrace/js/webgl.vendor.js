0c87f843e532da8d7dbcda322915d31cf1274f0b
lovebrownie <91576261+lovebrownie@users.noreply.github.com>
2026-06-29 11:50:53 +0200

    Fix FPScanner language and WebGL worker mismatches.
    - Sync navigator.language with navigator.languages[0]
    - Inject matching WebGL spoof into blob Workers so worker context
      reports the same vendor/renderer as the main page

--- untrace/js/webgl.vendor.js
+++ untrace/js/webgl.vendor.js
@@ -1,28 +1,57 @@
 // https://github.com/berstend/puppeteer-extra/blob/c44c8bb0224c6bba2554017bfb9d7a1d0119f92f/packages/puppeteer-extra-plugin-stealth/evasions/webgl.vendor/index.js
 
 (vendor, renderer) => {
+  const spoofVendor = vendor || 'Intel Inc.'
+  const spoofRenderer = renderer || 'Intel Iris OpenGL Engine'
+  const UNMASKED_VENDOR_WEBGL = 37445
+  const UNMASKED_RENDERER_WEBGL = 37446
+
   const getParameterProxyHandler = {
     apply: function (target, ctx, args) {
       const param = (args || [])[0]
-      // UNMASKED_VENDOR_WEBGL
-      if (param === 37445) {
-        return vendor || 'Intel Inc.' // default in headless: Google Inc.
+      if (param === UNMASKED_VENDOR_WEBGL) {
+        return spoofVendor
       }
-      // UNMASKED_RENDERER_WEBGL
-      if (param === 37446) {
-        return renderer || 'Intel Iris OpenGL Engine' // default in headless: Google SwiftShader
+      if (param === UNMASKED_RENDERER_WEBGL) {
+        return spoofRenderer
       }
       return utils.cache.Reflect.apply(target, ctx, args)
     }
   }
 
-  // There's more than one WebGL rendering context
-  // https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext#Browser_compatibility
-  // To find out the original values here: Object.getOwnPropertyDescriptors(WebGLRenderingContext.prototype.getParameter)
   const addProxy = (obj, propName) => {
+    if (!obj) {
+      return
+    }
     utils.replaceWithProxy(obj, propName, getParameterProxyHandler)
   }
-  // For whatever weird reason loops don't play nice with Object.defineProperty, here's the next best thing:
+
   addProxy(WebGLRenderingContext.prototype, 'getParameter')
   addProxy(WebGL2RenderingContext.prototype, 'getParameter')
+
+  const workerPatch =
+    `(function(){var v=${JSON.stringify(spoofVendor)};var r=${JSON.stringify(spoofRenderer)};` +
+    `function p(proto){if(!proto)return;var o=proto.getParameter;proto.getParameter=function(n){` +
+    `if(n===37445)return v;if(n===37446)return r;return o.apply(this,arguments);};}` +
+    `p(typeof WebGLRenderingContext!=='undefined'?WebGLRenderingContext.prototype:null);` +
+    `p(typeof WebGL2RenderingContext!=='undefined'?WebGL2RenderingContext.prototype:null);})();`
+
+  const NativeWorker = Worker
+  function UntraceWorker(scriptURL, options) {
+    if (typeof scriptURL === 'string' && scriptURL.startsWith('blob:')) {
+      try {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', scriptURL, false)
+        xhr.send(null)
+        const blob = new Blob([workerPatch + xhr.responseText], {
+          type: 'application/javascript'
+        })
+        scriptURL = URL.createObjectURL(blob)
+      } catch (_) {}
+    }
+    return new NativeWorker(scriptURL, options)
+  }
+
+  utils.replaceProperty(globalThis, 'Worker', { value: UntraceWorker })
+  utils.patchToString(UntraceWorker, 'function Worker() { [native code] }')
 }
